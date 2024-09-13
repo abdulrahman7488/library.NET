9a1245275aee20865d5897d78fe02dc4d7f9b156
abdulrahman7488 <abdulrahmanmohamed7488@gmail.com>
2024-09-13 19:15:57 +0300

    1

--- library1/library1/Controllers/HomeController.cs
+++ library1/library1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using library1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,11 @@ namespace library1.Controllers
 {
     public class HomeController : Controller
     {
+        private libraryEntities3 db = new libraryEntities3();
         public ActionResult Index()
         {
-            return View();
+            var categories = db.Categories.ToList();
+            return View(categories);
         }
 
     }
